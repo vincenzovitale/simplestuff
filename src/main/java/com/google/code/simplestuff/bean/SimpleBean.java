349954b292a9053e8609ba87765dd3b94531986d
vincenzo.vitale <vincenzo.vitale@3fb36193-8351-0410-b11b-89f3b1086f8b>
2008-10-23 12:23:21 +0000

    Returning false when a bean in the equals is not a business object

--- src/main/java/com/google/code/simplestuff/bean/SimpleBean.java
+++ src/main/java/com/google/code/simplestuff/bean/SimpleBean.java
@@ -1,18 +1,18 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.google.code.simplestuff.bean;
 
@@ -81,8 +81,9 @@ public class SimpleBean {
 
         if ((firstBusinessObjectInfo == null)
                 || (secondBusinessObjectInfo == null)) {
-            throw new IllegalArgumentException(
-                    "One or both of the beans to compare are not annotated in their hierarchy as Business Object!!!");
+            return false;
+            // throw new IllegalArgumentException(
+            // "One or both of the beans to compare are not annotated in their hierarchy as Business Object!!!");
         }
 
         // TODO: Revise this code in order to make it more readable...
